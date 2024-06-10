9cf9c5caa47c3227073c8f0782164c6e22d62b0b
1ncakola <francisco.castillo@keyin.com>
2024-06-10 18:59:54 -0230

    qap2.js edited (2)

--- qap2.js
+++ qap2.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const EventEmitter = require('events');
 
 const server = http.createServer((req, res) => {
     const { url } = req;
@@ -24,7 +25,7 @@ const server = http.createServer((req, res) => {
 });
 
 function servePage(res, page) {
-    const filePath = `${__dirname}/views/${page}`;
+    const filePath = `${__dirname}/FullStackQAP2/${page}`;
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
@@ -42,9 +43,9 @@ function servePage(res, page) {
 const server2 = http.createServer((req, res) => {
     let filePath = '.' + req.url;
     if (filePath === './') {
-        filePath = `${__dirname}/views/index.html`;
+        filePath = `${__dirname}/FullStackQAP2/index.html`;
     } else {
-        filePath = `${__dirname}/views${req.url}.html`;
+        filePath = `${__dirname}/FullStackQAP2${req.url}.html`;
     }
 
     fs.readFile(filePath, (err, data) => {
@@ -66,6 +67,68 @@ const server2 = http.createServer((req, res) => {
     });
 });
 
+class MyEmitter extends EventEmitter {};
+const myEmitter = new MyEmitter ();
+
+myEmitter.on ('status', (code) => {
+    console.log(`Status Code: ${code}`);
+});
+
+myEmitter.on('routeAccess', (route) => {
+    console.log(`Route Access: ${route}`);
+});
+
+
+const server3 = http.createServer((req, res) => {
+    const {url} = req;
+
+    if(url === '/about') {
+        myEmitter.emit('routeAccess', '/about');
+    }
+
+    switch (url) {
+        case '/about':
+            servePage(res, 'about.html');
+            break;
+        case '/contact':
+            servePage(res, 'contact.html');
+            break;
+        case '/products':
+            servePage(res, 'products.html');
+            break;
+        case '/subscribe':
+            servePage(res, 'subscribe.html');
+            break;
+        default:
+            servePage(res, 'index.html');
+    }
+});
+
+function servePage(res, page) {
+    const filePath = `${__dirname}/FullStackQAP2/${page}`;
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Page not found');
+                myEmitter.emit('status', 404); 
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+                myEmitter.emit('status', 500); 
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+            myEmitter.emit('status', 200); 
+        }
+    });
+}
+
+
+
+
+
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
@@ -74,4 +137,9 @@ server.listen(PORT, () => {
 const PORT2 = 3001;
 server2.listen(PORT2, () => {
     console.log(`Server is listening on port ${PORT2}`);
+});
+
+const PORT3 = 3002;
+server3.listen(PORT3, () => {
+    console.log(`Server is listening on port ${PORT3}`);
 });
